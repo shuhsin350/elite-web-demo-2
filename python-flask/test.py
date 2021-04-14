93947649aa409f49ad699d59595eab8c7b979412
shuhsin <dear88367@gmail.com>
2021-04-14 16:03:28 +0800

    改變位置

---
+++ python-flask/test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return 'hello test!'
+    
+if __name__=="__main__":
+    app.run()
