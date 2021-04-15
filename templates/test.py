8311bad156e5c1894c82aab0ce600f2480d2d5bc
shuhsin <dear88367@gmail.com>
2021-04-15 15:52:03 +0800

    add

--- templates/test.py
+++ templates/test.py
@@ -1,12 +1,15 @@
-from flask import Flask
-from flask import render_python-flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/hello')
+@app.route('/hello', defaults={'name': 'Someone'}) #為參數加上預設值
 @app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello(name): #同一個 function 可套用多組路由
+    return 'Hello, {}!'.format(name)
+
+@app.route('/')
+def main(name=None):
+    # a = 'World'
+    return render_template('index.html', name='World')
+
+
