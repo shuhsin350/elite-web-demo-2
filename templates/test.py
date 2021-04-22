fc1900de2b24a2ce2f4860053145881954346ce1
shuhsin <dear88367@gmail.com>
2021-04-22 15:27:20 +0800

    雜物

--- templates/test.py
+++ templates/test.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello', defaults={'name': 'Someone'}) #為參數加上預設值
-@app.route('/hello/<name>')
-def hello(name): #同一個 function 可套用多組路由
-    return 'Hello, {}!'.format(name)
-
 @app.route('/')
-def main(name=None):
-    # a = 'World'
-    return render_template('index.html', name='World')
+def index():
+    # 顯示表單
+    return render_template('form.html')
+
+
+@app.route('/', methods=['POST'])
+def process():
+    # 處理圖片
+    return 'Process'
+
 
 
